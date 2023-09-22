d8fa951bf909ee3532c8cd03d52acb8f75e59c8b
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-09-22 20:30:02 +0100

    removed unnecessary false statements and added the try block in a better part of the code in console.py

--- console.py
+++ console.py
@@ -115,30 +115,30 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        try:
-            "create an object for a given class"
-            if not args:
-                print("** class name missing **")
-                return False
+        "create an object for a given class"
+        if not args:
+            print("** class name missing **")
+            return
 
-            args_list = shlex.split(args)
-            class_name = args_list[0]
+        args_list = shlex.split(args)
+        class_name = args_list[0]
 
-            if class_name not in HBNBCommand.classes:
-                print("** class doesn't exist **")
-                return False
+        if class_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
 
-            new_instance = HBNBCommand.classes[class_name]()
+        new_instance = HBNBCommand.classes[class_name]()
 
-            for arg in args_list[1:]:
-                param_value = arg
-                param_value = arg.split("=")
-                param = param_value[0]
-                value = param_value[1]
+        for arg in args_list[1:]:
+            param_value = arg
+            param_value = arg.split("=")
+            param = param_value[0]
+            value = param_value[1]
 
-                # replace _ with a space and remove any double quotes
-                value = value.replace("_", " ").replace('"', '\\"')
+            # replace _ with a space and remove any double quotes
+            value = value.replace("_", " ").replace('"', '\\"')
 
+            try:
                 if '.' in value:
                     value = float(value)
                     setattr(new_instance, param, value)
@@ -147,11 +147,12 @@ class HBNBCommand(cmd.Cmd):
                     setattr(new_instance, param, value)
                 else:
                     setattr(new_instance, param, value)
+            except Exception:
+                pass
 
-            new_instance.save()
-            print(new_instance.id)
-        except ValueError:
-            pass
+        new_instance.save()
+        print(new_instance.id)
+
 
     def help_create(self):
         """ Help information for the create method """
