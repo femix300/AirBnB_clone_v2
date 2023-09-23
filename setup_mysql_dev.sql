f7d23a7692440f8c3d8691856cf14073bf659177
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-09-23 11:27:27 -0700

    Some changes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,5 @@
 -- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grant privileges to the user for hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- Grant SELECT privilege to the user for performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
