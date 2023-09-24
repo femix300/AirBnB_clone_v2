2c60ea23681cc62ebeddec47773bfc601083ac58
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-09-24 03:18:04 +0100

    changed logic in user.py

--- models/user.py
+++ models/user.py
@@ -6,13 +6,23 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.place import Place
 from models.review import Review
+import os
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
     __tablename__ = 'users'
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    storage_type = os.getenv("HBNB_TYPE_STORAGE")
+
+    if storage_type == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+    
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
